a651d69a7d08cee29cc8e6fe29a5be3f402f0c36
JoJoseB <bezerra909@gmail.com>
2023-09-14 04:05:57 -0300

    adicionando aquivos base

---
+++ ORDENACAO_POO/prog.py
@@ -0,0 +1,23 @@
+from CLASSES import LISTA
+
+'''var = LISTA()
+print(var.EXIBIR_LISTA())
+var('Bubble')
+print(var.EXIBIR_LISTA())
+
+var2 = LISTA()
+print(var2.EXIBIR_LISTA())
+var2('insertion')
+print(var2.EXIBIR_LISTA())
+
+var3 = LISTA()
+print(var3.EXIBIR_LISTA())
+var3('selection')
+print(var3.EXIBIR_LISTA())
+
+
+'''
+var = LISTA()
+print(var.EXIBIR_LISTA())
+var('BUBBLE')
+print(var.EXIBIR_LISTA())
