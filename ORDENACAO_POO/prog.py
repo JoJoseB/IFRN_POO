ab69e02c6fa225f3716e51b36367d91b2d022d16
JoJoseB <bezerra909@gmail.com>
2023-09-15 12:49:44 -0300

    salvar-adicionado

--- ORDENACAO_POO/prog.py
+++ ORDENACAO_POO/prog.py
@@ -1,5 +1,7 @@
 #from CLASSES import LISTA
 import os
+from CLASSES import LISTA
+
 
 '''var = LISTA()
 print(var.EXIBIR_LISTA())
@@ -15,11 +17,7 @@ var3 = LISTA()
 print(var3.EXIBIR_LISTA())
 var3('selection')
 print(var3.EXIBIR_LISTA())
-
-var = LISTA()
-print(var.EXIBIR_LISTA())
-var('insertion')
-print(var.EXIBIR_LISTA())
 '''
-caminho_prog = os.path.dirname(__file__)
-print(caminho_prog)
+var = LISTA()
+var.EXIBIR_LISTA()
+var.SALVAR()
