931a4c0a20d3d2abb2410e2a65749460de69d5c6
Jose Bezerra de Araujo Neto <20222014050021@ifrn.local>
2023-10-09 15:52:19 -0300

    update-classes.py

--- ORGANIZADOR_COLECAO/classes.py
+++ ORGANIZADOR_COLECAO/classes.py
@@ -3,17 +3,26 @@ class Pessoa:
         self.__nome = nome
         self.__cpf = cpf
         self.__email = email
-
-    def dados(self):
+    
+    def _dados(self):
         return {self.__cpf:{'NOME':self.__nome,'E-MAIL':self.__email}}
 
 class Aluno:
     def __init__(self,pessoa):
-        self.__pessoa = pessoa.dados()
-        self.__pessoa['MAT'] = []
-        print(self.__pessoa)
+        self.__pessoa = pessoa._dados()
+        self.var = list(self.__pessoa.keys())[0]
+        print(self.__pessoa[self.var])
+        if 'MAT' not in self.__pessoa[self.var]:
+            self.__pessoa[self.var]['MAT'] = []
+        else:
+            print('Aluno já cadastrado')
 
     def matricula(self,mat):
-        self.__pessoa['MAT'].append(mat)
-        print(self.__pessoa)
+        if mat in self.__pessoa[self.var]['MAT']:
+            print(f'{mat} já associado ao CPF {self.var}')
+        else:
+            self.__pessoa[self.var]['MAT'].append(mat)
+        return self.__pessoa
+    
+    def _dados(self):
         return self.__pessoa
--- ORGANIZADOR_COLECAO/main.py
+++ ORGANIZADOR_COLECAO/main.py
@@ -3,6 +3,15 @@ from classes import *
 
 pessoa1 = Pessoa('jose','123.456.678-90','email@email.com')
 
+print(pessoa1._dados())
 aluno1 = Aluno(pessoa1)
-aluno1.matricula('0000000000')
-aluno1.matricula('0000000001')
+print(aluno1._dados())
+aluno1.matricula('01')
+print(aluno1._dados())
+aluno1.matricula('02')
+print(aluno1._dados())
+aluno2 = Aluno(aluno1)
+print(aluno2._dados())
+aluno2.matricula('03')
+print(aluno1._dados())
+print(aluno2._dados())
