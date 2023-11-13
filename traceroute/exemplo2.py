94e8b37560f855535deebc57b8ff9c8ea37693b2
José Bezerra <121698681+JoJoseB@users.noreply.github.com>
2023-11-13 17:30:40 +0000

    main2-exemplo4

--- traceroute/exemplo2.py
+++ traceroute/exemplo2.py
@@ -6,22 +6,23 @@ import socket
 import time
 import struct
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-s.bind(('192.168.0.11', socket.IPPROTO_IP))
-s.ioctl(socket.SIO_RCVALL, socket.RCVALL_MAX)
+interface = socket.gethostbyname(socket.gethostname())
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+s.bind((interface,socket.IPPROTO_TCP))
+timeout = time.time() + 5
 
-count = 0
 while True:
-    count += 1
     pkt,origem = s.recvfrom(1024)
     print(pkt)
-    if count == 1:
+    if time.time() > timeout:
         break
 
 tam = len(pkt)
 
 version_ihl, dscp_ecn, t_len, id, flag_frag,ttl, proto, chksum, src_ip, src_ip2, src_ip3, src_ip4, dst_ip, dst_ip2, dst_ip3, dst_ip4 = struct.unpack(f'! 2b 3H B b H 8B', pkt[0:20])
 print(version_ihl, dscp_ecn, t_len, id, flag_frag, ttl, proto, chksum, src_ip,src_ip2,src_ip3,src_ip4, dst_ip, dst_ip2, dst_ip3, dst_ip4)
+
+
 '''rv_dest_ip = struct.pack(f'4B',dst_ip, dst_ip2, dst_ip3, dst_ip4)'''
 '''print(rv_dest_ip)
 teste = socket.inet_ntoa(rv_dest_ip)
---
+++ traceroute/main2.py
@@ -0,0 +1,4 @@
+from exemplo4 import SNIFFER
+
+teste = SNIFFER('127.0.0.1')
+teste.getpackets(5)
